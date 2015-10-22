ccd978008a89ea43108341f9e48cfb8e45c9d14f
Matt Luker <matt.luker001@gmail.com>
2015-10-22 00:44:57 -0500

    finish base frame for app.

--- leaderboard.js
+++ leaderboard.js
@@ -1,18 +1,25 @@
 PlayersList = new Meteor.Collection('players');
 
-
+//Create Client side js viewable by browser.
 if(Meteor.isClient) {
   Template.leaderboard.player = function() {
+    //Create player and sort by points, then by name
 return PlayersList.find({}, {sort: {score: -1, name: 1}});
   }
 Template.leaderboard.selectedClass = function(){
-  //return 'selected'
+  //return 'selected when clicked'
   var playerId = this._id;
   var selectedPlayer = Session.get('selectedPlayer');
   if(selectedPlayer === playerId) {
     return 'selected'
   }
 }
+  Template.leaderboard.showSelectedPlayer = function() {
+    //function to show current selected player under the add points button
+    var selectedPlayer = Session.get('selectedPlayer');
+    return PlayersList.findOne(selectedPlayer);
+  }
+
   Template.leaderboard.events({
     'click li.player': function(){
       var playerId = this._id;
@@ -23,9 +30,30 @@ Template.leaderboard.selectedClass = function(){
     'click #increment': function(){
     var selectedPlayer = Session.get('selectedPlayer');
     PlayersList.update({_id: selectedPlayer}, {$inc: {score: 5}});
-    }
+  },
+  'click #decrement': function(){
+  var selectedPlayer = Session.get('selectedPlayer');
+  PlayersList.update({_id: selectedPlayer}, {$inc: {score: -5}});
+},
+  'click #remove': function() {
+  var selectedPlayer =  Session.get('selectedPlayer');
+  PlayersList.remove(selectedPlayer);
+  }
   });
-
+//Create add button details. 1. preventDefault 2. using submit form for .on function instead
+//of click so client can use alternative submit options.
+Template.addPlayerForm.events({
+  'submit form': function(event, template) {
+    event.preventDefault();
+    var playerNameVar = template.find('#playerName').value;
+    PlayersList.insert({
+      name: playerNameVar,
+      score: 0
+    });
+    console.log(event.type);
+    console.log(playerNameVar);
+  }
+});
 }
 if(Meteor.isServer) {
 }
